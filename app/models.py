c14dad8907b23c3db6799de17a3857944be171bd
tylerbeckb <tylerbbrown@icloud.com>
2023-11-22 17:12:10 +0000

    fix: database made

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
 from app import db
 
 class User(db.Model):
-    username = db.Coloumn(db.String(100), index = True, unique = True, primary_key = True)
-    password = db.Coloumn(db.String(100))
+    username = db.Column(db.String(100), index = True, unique = True, primary_key = True)
+    password = db.Column(db.String(100))
 
     def __repr__(self):
         return f"User('{self.username}','{self.password}')"
 
 class Reviews(db.Model):
-    title = db.Coloumn(db.String(100))
+    reviewId = db.Column(db.Integer, index = True, unique = True, primary_key = True)
+    title = db.Column(db.String(100))
     review = db.Column(db.Text)
     date = db.Column(db.DateTime)
     rating = db.Column(db.Integer)
@@ -19,7 +20,7 @@ class Reviews(db.Model):
         return f"User('{self.title}','{self.review}','{self.date}','{self.rating}')"
 
 class Stadiums(db.Model):
-    name = db.Coloumn(db.String(100), index = True, unique = True, primary_key = True)
+    name = db.Column(db.String(100), index = True, unique = True, primary_key = True)
     club = db.Column(db.String(100))
 
     def __repr__(self):
