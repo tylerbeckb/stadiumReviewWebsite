3d11bfa464a021c590832cd005ca32b3eedb7ed4
tylerbeckb <tylerbbrown@icloud.com>
2023-11-23 16:34:42 +0000

    fix: commented out csv reader

--- app/views.py
+++ app/views.py
@@ -3,22 +3,25 @@ from app import app, db
 from .form import SearchForm
 from app.models import User, Reviews, Stadiums
 from app.models import User, Reviews, Stadiums
-import csv, os
+import csv
+import os
 
 
 
 @app.route('/')
 def index():
+    '''
     url_for('static',filename='stadium.csv')
-    csv_file = os.path.join(app.root_path, 'static', 'stadium.csv')
-    with open(csv_file, "r") as csv:
-        reader = csv.reader(csv, skipinitialspace=True)
+    csvPath = os.path.join(app.root_path, 'static', 'stadium.csv')
+    with open(csvPath, "r") as csvFile:
+        reader = csv.reader(csvFile, skipinitialspace=True)
         for row in reader:
             name = row[0]
             club = row[1]
             record = Stadiums(name = name, club = club)
             db.session.add(record)
             db.session.commit()
+    '''
     searchForm = SearchForm()
     if searchForm.validate_on_submit():
         searchName = request.form['stadName']
