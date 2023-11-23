ca79eedec32bc75dc1d965495bbdd616c2430a8e
tylerbeckb <tylerbbrown@icloud.com>
2023-11-23 17:16:49 +0000

    feat:imported admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_babel import Babel
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,4 +11,6 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+admin = Admin(app,template_mode='bootstrap4')
+babel = Babel(app)
 from app import views, models
