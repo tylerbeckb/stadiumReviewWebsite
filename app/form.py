9ed062768e9a50a05411a2edf5a029ceb14acb67
tylerbeckb <tylerbbrown@icloud.com>
2023-11-26 11:56:55 +0000

    feat: login and sign up form

--- app/form.py
+++ app/form.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, default
-from wtforms.validators import DataRequired, Length, default
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
 
 class SearchForm(FlaskForm):
-    stadName = StringField('stadName', validators = [DataRequired(), Length(min = 0, max = 100), default='Enter Stadium'])
+    stadName = StringField('stadName', validators = [DataRequired(), Length(min = 0, max = 100)])
+
+class SignupForm(FlaskForm):
+    signName = StringField('signName', validators = [DataRequired(), Length(min = 0, max = 100)])
+    signPassword = StringField('signPassword', validators = [DataRequired(), Length(min = 0, max = 100)])
+
+class LoginForm(FlaskForm):
+    loginName = StringField('loginName', validators = [DataRequired(), Length(min = 0, max = 100)], render_kw={"placeholder": "UserName"})
+    loginPassword = StringField('loginPassword', validators = [DataRequired(), Length(min = 0, max = 100)], render_kw={"placeholder": "Password"})
