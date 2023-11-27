29e252752c51fd5e70fbf1d43d6da338786da1af
tylerbeckb <tylerbbrown@icloud.com>
2023-11-27 12:08:11 +0000

    upgraded db to include id as primary key

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
+#from flask_login import UserMixin
 
 class User(db.Model):
-    username = db.Column(db.String(100), index = True, unique = True, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), index = True, unique = True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
 
@@ -9,19 +11,20 @@ class User(db.Model):
         return f"User('{self.username}','{self.password}')"
 
 class Reviews(db.Model):
-    reviewId = db.Column(db.Integer, index = True, unique = True, primary_key = True)
+    id = db.Column(db.Integer, index = True, unique = True, primary_key = True)
     title = db.Column(db.String(100))
     review = db.Column(db.Text)
     date = db.Column(db.DateTime)
     rating = db.Column(db.Integer)
-    accountUsername = db.Column(db.String(100), db.ForeignKey('user.username'))
-    stadiumName = db.Column(db.String(100), db.ForeignKey('stadiums.name'))
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    stadiumId = db.Column(db.Integer, db.ForeignKey('stadiums.id'))
 
     def __repr__(self):
         return f"Review('{self.title}','{self.review}','{self.date}','{self.rating}')"
 
 class Stadiums(db.Model):
-    name = db.Column(db.String(100), index = True, unique = True, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), index = True, unique = True)
     club = db.Column(db.String(100))
 
     def __repr__(self):
