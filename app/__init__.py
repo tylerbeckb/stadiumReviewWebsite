854d30bca23c23fb23904b1ea4c404931876ee29
tylerbeckb <tylerbbrown@icloud.com>
2023-11-27 10:55:04 +0000

    fix: can now add/delted rows in db

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 admin = Admin(app,template_mode='bootstrap4')
 babel = Babel(app)
