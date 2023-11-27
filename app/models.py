77f21afe90c501b4a2e483b04c17e716f1999db8
tylerbeckb <tylerbbrown@icloud.com>
2023-11-27 16:51:30 +0000

    feat: used usermixin

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
-#from flask_login import UserMixin
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), index = True, unique = True)
     password = db.Column(db.String(100))
