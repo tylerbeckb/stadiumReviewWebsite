018030655011001d9dd78c8c4c0214df9df21355
tylerbeckb <tylerbbrown@icloud.com>
2023-11-28 16:01:52 +0000

    feat: fields for review form

--- app/form.py
+++ app/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, RadioField, DateField
 from wtforms.validators import DataRequired, Length
+from wtforms.widgets import TextArea
 
 class SearchForm(FlaskForm):
     stadName = StringField('stadName', validators = [DataRequired(), Length(min = 0, max = 100)])
@@ -13,3 +14,10 @@ class SignupForm(FlaskForm):
 class LoginForm(FlaskForm):
     loginName = StringField('loginName', validators = [DataRequired(), Length(min = 0, max = 100)], render_kw={"placeholder": "Username"})
     loginPassword = StringField('loginPassword', validators = [DataRequired(), Length(min = 0, max = 100)], render_kw={"placeholder": "Password"})
+
+class ReviewForm(FlaskForm):
+    stadName = StringField('stadName', validators = [DataRequired()])
+    rating = RadioField('rating', choices=[('1','☆'),('2','☆'),('3','☆'),('4','☆'),('5','☆')])
+    title = StringField('title', validators = [DataRequired(), Length(min = 0, max = 100)], render_kw={"placeholder": "Title of Review"})
+    date = DateField('date', validators = [DataRequired()])
+    reviewText = StringField('reviewText',  validators = [DataRequired()], widget = TextArea(), render_kw={"placeholder": "Tell us about your visit"})
