046c19953915c7ac6b1000677af3bf8499369447
tylerbeckb <tylerbbrown@icloud.com>
2023-12-02 14:50:09 +0000

    fix: hides icons

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var csrf_token = $('meta[name=csrf-token]').attr('content');
+    $(".hidden").hide()
     $.ajaxSetup({
         beforeSend: function(xhr, settings) {
             if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
@@ -11,16 +12,26 @@ $(document).ready(function() {
     $("a.vote").on("click", function() {
         var clicked_obj = $(this);
 
-        var review_id = $(this).attr('id');
+        var reviewId = $(this).attr('id');
+        var voteType = $(this).children()[0].id;
 
         $.ajax({
             url: '/vote',
             type: 'POST',
-            data: JSON.stringify({ review_id: review_id }),
+            data: JSON.stringify({ reviewId: reviewId, voteType: voteType }),
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function(response){
                 console.log(response)
+
+                if(voteType == "not") {
+                    $("#liked").show()
+                    $("#not").hide()
+                }
+                else {
+                    $("#liked").hide()
+                    $("#not").show()
+                }
             },
             error: function(error){
                 console.log(error);
