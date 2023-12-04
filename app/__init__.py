0ccae42be156f89d2715add72807c5d417fb0bd4
tylerbeckb <tylerbbrown@icloud.com>
2023-12-04 11:35:46 +0000

    feat: configured logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_babel import Babel
+from logging.config import dictConfig
 
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "flask.log",
+                "formatter": "default",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+        }
+)
 
 app = Flask(__name__)
 app.config.from_object('config')
